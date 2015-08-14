4eb98d961c26c6f9f0df04c97c4141cee6949cb0
scuta <scutak.o@gmail.com>
2015-08-14 13:22:12 +0800

    重新划分目录上传

--- CardRelease/src/test/java/com/sanploy/card/report/IReportFileExporter.java
+++
@@ -1,9 +0,0 @@
-package com.sanploy.card.report;
-
-import java.io.OutputStream;
-
-import net.sf.jasperreports.engine.JRException;
-
-public interface IReportFileExporter {
-	public void export(ReportPrint reportPrint, OutputStream os) throws JRException;
-}
